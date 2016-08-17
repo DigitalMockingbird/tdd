05540156c9ded589ec1bcbcbbfe79ed9e381e201
DigitalMockingbird <noah@digitalmockingbird.com>
2016-08-17 16:24:25 -0700

    Refactoring bad code smell to enforce DRY principals

--- functional_tests.py
+++ functional_tests.py
@@ -34,24 +34,26 @@ class NewVisitorTest(unittest.TestCase):
             # "1: Buy peacock feathers" as an item of the to-do list
             inputbox.send_keys(Keys.ENTER)
 
-            table = self.browser.find_element_by_id('id_list_table')
             rows = self.browser.find_elements_by_tag_name('tr')
-            self.assertTrue(
-                any(row.text == '1: Buy peacock feathers' for row in rows),
-                "New to-do item did not appear in table"
-            )
+
+            self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
             # There is another text box inviting them to add another item.
             # They enter "Use peacock feathers to make a fly" (they're very methodical)
-            self.fail('Finish the test!')
+            inputbox = self.browser.find_element_by_id('id_new_item')
+            inputbox.send_keys('Use peacock feather to make a fly')
+            inputbox.send_keys(Keys.ENTER)
 
             # The page updates again, and now shows both items on their list
-
+            table = self.browser.find_element_by_id('id_list_table')
+            rows = table.find_element_by_tag_name('tr')
+            self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+            self.assertIn('2: Use peacock feather to make a fly', [row.text for row in rows])
 
             # The person wonders if the site will retain the information. They see
             # the site has generated a unique url for them -- there is some explanatory
             # text for the effect.
-
+            self.fail('Finish the test')
 
             # They visit the url and the to-do list is still there
 
