2abaf48ca651f2a562b37fb332dc7c993cdd1588
DigitalMockingbird <noah@digitalmockingbird.com>
2016-08-27 13:21:57 -0700

    New URL, view and template to display lists. Changed webdriver to chrome until Selenium supports firefox 48

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,83 +3,81 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(LiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome('/Users/crux/Downloads/chromedriver')
+        self.browser.implicitly_wait(3)
 
-        def setUp(self):
-           self.browser = webdriver.Firefox()
-           self.browser.implicitly_wait(3)
+    def tearDown(self):
+        self.browser.quit()
 
-        def tearDown(self):
-            self.browser.quit()
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
-        def check_for_row_in_list_table(self, row_text):
-            table = self.browser.find_element_by_id('id_list_table')
-            rows = table.find_elements_by_tag_name('tr')
-            self.assertIn(row_text, [row.text for row in rows])
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # User has about about a cool online to-do app. They go to check the homepage.
+        self.browser.get(self.live_server_url)
 
-        def test_can_start_a_list_and_retrieve_it_later(self):
-            # User has about about a cool online to-do app. They go to check the homepage.
-            self.browser.get(self.live_server_url)
+        # They notice the title and the header mentions to-do lists.
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-            # They notice the title and the header mentions to-do lists.
-            self.assertIn('To-Do', self.browser.title)
-            header_text = self.browser.find_element_by_tag_name('h1').text
-            self.assertIn('To-Do', header_text)
+        # They're invited to do a to-do list right away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
-            # They're invited to do a to-do list right away
-            inputbox = self.browser.find_element_by_id('id_new_item')
-            self.assertEqual(
-                inputbox.get_attribute('placeholder'),
-                'Enter a to-do item'
-            )
+        # They type "Buy peacock feathers" into the text box (they're a fly-fisher).
+        inputbox.send_keys('Buy peacock feathers')
 
-            # They type "Buy peacock feathers" into the text box (they're a fly-fisher).
-            inputbox.send_keys('Buy peacock feathers')
+        # When they hit enter the page updates, and now lists
+        # "1: Buy peacock feathers" as an item of the to-do list
+        inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-            # When they hit enter the page updates, and now lists
-            # "1: Buy peacock feathers" as an item of the to-do list
-            inputbox.send_keys(Keys.ENTER)
-            edith_list_url = self.browser.current_url
-            self.assertRegex(edith_list_url, '/lists/.+')
-            self.check_for_row_in_list_table('1: Buy peacock feathers')
+        # There is another text box inviting them to add another item.
+        # They enter "Use peacock feathers to make a fly" (they're very methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
-            # There is another text box inviting them to add another item.
-            # They enter "Use peacock feathers to make a fly" (they're very methodical)
-            inputbox = self.browser.find_element_by_id('id_new_item')
-            inputbox.send_keys('Use peacock feathers to make a fly')
-            inputbox.send_keys(Keys.ENTER)
+        # The page updates again, and now shows both items on their list
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-            # The page updates again, and now shows both items on their list
-            self.check_for_row_in_list_table('1: Buy peacock feathers')
-            self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-
-            # Now a new user, Fancis, comes along to the site
+        # Now a new user, Fancis, comes along to the site
 
 
-            # We use a new browser session to make sure that no information of Edith's
-            # is coming through from cookies etc
-            self.browser.quit()
-            self.browser = webdriver.Firefox()
+        # We use a new browser session to make sure that no information of Edith's
+        # is coming through from cookies etc
+        self.browser.quit()
+        self.browser = webdriver.Chrome('/Users/crux/Downloads/chromedriver')
 
-            # Francis visits the home page. There is no sign of Edith's list
-            self.browser.get(self.live_server_url)
-            page_text = self.browser.find_element_by_tag_name('body').text
-            self.assertNotIn('Buy peacock feathers', page_text)
-            self.assertNotIn('make a fly', page_text)
+        # Francis visits the home page. There is no sign of Edith's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
 
-            # Francis starts a new list by entering a new item
-            # He is less interesting than Edith..
-            inputbox = self.browser.find_element_by_id('id_new_item')
-            inputbox.send_keys('Buy milk')
-            inputbox.send_keys(Keys.ENTER)
+        # Francis starts a new list by entering a new item
+        # He is less interesting than Edith..
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
 
-            # Francis gets his own unique URL
-            francis_list_url = self.browser.current_url
-            self.assertRegex(francis_list_url, '/lists/.+')
-            self.assertNotEqual(francis_list_url, edith_list_url)
+        # Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
 
-            # Again there is no trace of Edith's list
-            self.assertNotIn('Buy peacock feathers', page_text)
-            self.assertIn('Buy milk', page_text)
+        # Again there is no trace of Edith's list
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
 
-            # Satisfied, they both go back to sleep
-
+        # Satisfied, they both go back to sleep
--- superlists/urls.py
+++ superlists/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from lists.views import home_page
+from lists.views import home_page, view_list
 
 urlpatterns = [
         url(r'^$', home_page, name='home'),
+        url(r'^lists/the-only-list-in-the-world/$', view_list, name='view_list'),
         # url(r'^admin/', include(admin.site.urls)),
 ]
