a998a41135dd780d421b153038d988ca95627d15
DigitalMockingbird <noah@digitalmockingbird.com>
2016-08-28 14:02:10 -0700

    refactor templates to use a base template

--- superlists/settings.py
+++ superlists/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# INTERNAL_IPS = ('127.0.0.1', )
 
 # Application definition
 
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'superlists.urls'
