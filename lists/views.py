9ecc59d0935e24ac4d57b0b2cd501e60d705e1ca
DigitalMockingbird <noah@digitalmockingbird.com>
2016-09-16 16:56:13 -0700

    Refactor list view to handle new item POSTS

--- lists/views.py
+++ lists/views.py
@@ -9,12 +9,29 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    return render(request, 'list.html', {'list': list_})
+    error = None
+
+    if request.method == 'POST':
+        try:
+            item = Item.objects.create(
+                text=request.POST['item_text'],
+                list=list_
+            )
+            item.full_clean()
+            item.save()
+            return redirect('/lists/{0}/'.format(list_.id, ))
+        except ValidationError:
+            error = "You can't have an empty list item"
+
+    return render(request, 'list.html', {'list': list_, 'error': error})
 
 
 def new_list(request):
     list_ = List.objects.create()
-    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item.objects.create(
+        text=request.POST['item_text'],
+        list=list_
+    )
     try:
         item.full_clean()
         item.save()
--- superlists/urls.py
+++ superlists/urls.py
@@ -20,7 +20,6 @@ from lists.views import home_page, view_list, new_list, add_item
 urlpatterns = [
         url(r'^$', home_page, name='home'),
         url(r'^lists/(\d+)/$', view_list, name='view_list'),
-        url(r'^lists/(\d+)/add_item$', add_item, name='add_item'),
         url(r'^lists/new$', new_list, name='new_list'),
         # url(r'^admin/', include(admin.site.urls)),
 ]
