00dd974955e5c49fae03fb60eeb9f54228cfa0b3
DigitalMockingbird <noah@digitalmockingbird.com>
2016-10-18 16:25:59 -0700

    added lists url file. continuing from pg 279.

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from lists.views import new_list, view_list
+
+urlpatterns = [
+    # url(r'^(\d+)/$', view_list, name='view_list'),
+    # url(r'^new$', new_list, name='new_list'),
+]
--- superlists/urls.py
+++ superlists/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from lists.views import home_page, view_list, new_list, add_item
+from lists.views import home_page
 
 urlpatterns = [
         url(r'^$', home_page, name='home'),
-        url(r'^lists/(\d+)/$', view_list, name='view_list'),
-        url(r'^lists/new$', new_list, name='new_list'),
+        url(r'^lists/', include('lists.urls')),
+        url(r'^accounts/', include('accounts.urls')),
         # url(r'^admin/', include(admin.site.urls)),
 ]
